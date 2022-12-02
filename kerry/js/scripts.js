8f0bb0c8d7a8e6bedb5461f1cbbecf1d24104125
Catalufa <79006629+Catalufa@users.noreply.github.com>
2022-12-02 14:02:40 +0000

    Update scripts.js

--- kerry/js/scripts.js
+++ kerry/js/scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("open").addEventListener("click", function() {
   Swal.fire({title: "Loading present...", showConfirmButton: false});
   setTimeout(function () {
-    Swal.fire("Stickers!", "", "success")
+    Swal.fire("Pastel highlighter!", "", "success")
   }, 3000);
 })
