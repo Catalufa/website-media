9869125c0c3e662740dd6c9e9849910cb0e4fedf
Catalufa <79006629+Catalufa@users.noreply.github.com>
2022-12-04 12:31:31 +0000

    Update scripts.js

--- kerry/js/scripts.js
+++ kerry/js/scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("open").addEventListener("click", function() {
   Swal.fire({title: "Loading present...", showConfirmButton: false});
   setTimeout(function () {
-    Swal.fire("Mini blackboard!", "", "success")
+    Swal.fire("Hand cream!", "", "success")
   }, 3000);
 })
