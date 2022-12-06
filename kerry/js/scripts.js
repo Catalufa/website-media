069e7c90c4cf7577b5add196dd4f535077c3c56b
Catalufa <79006629+Catalufa@users.noreply.github.com>
2022-12-06 15:20:53 +0000

    Update scripts.js

--- kerry/js/scripts.js
+++ kerry/js/scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("open").addEventListener("click", function() {
   Swal.fire({title: "Loading present...", showConfirmButton: false});
   setTimeout(function () {
-    Swal.fire("Stickers pt 2!", "", "success")
+    Swal.fire("Glass trinket", "", "success")
   }, 3000);
 })
